404e2dc6fecc2491e84a9af7d44f303980553857
ying.chen <ying.chen@d1m.cn>
2014-03-13 14:14:27 +0800

    Added testing for staged files

--- lib/function.php
+++ lib/function.php
@@ -1,17 +1,5 @@
 <?php
-function test3(){
-//add in bak/yingfr
-}
-function test2(){
-
-}
 function test(){
     echo 'this is test()';
 }
-function test4(){
-//add test4() in www/yingfr
-}
-function test5(){
-}
-function test6(){
-}
+//more action
